200cc1375b63d2cf18d5b2e5f1d6ec305b6292ab
Marcos Silva <marcos.a.silva6@ba.docente.senai.br>
2023-06-26 21:34:24 -0300

    Add tela Sobre

--- minhas_financas/lib/modules/home/pages/about.dart
+++ minhas_financas/lib/modules/home/pages/about.dart
@@ -5,6 +5,46 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Center(
+          child: Text(
+            'SOBRE NÓS',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Image.asset('assets/images/sobre.png'),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 25, horizontal: 40),
+            child: Text.rich(
+              TextSpan(
+                text: 'A ',
+                children: [
+                  TextSpan(
+                    text: 'XPTO ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                        'é uma startup inovadora que está mudando a maneira como as pessoas pensam sobre desenvolvimento de aplicativos. Nós somos a Apple do mundo do desenvolvimento de aplicativos - sempre na vanguarda da tecnologia e liderando o caminho com nossos produtos e serviços de ponta.',
+                  )
+                ],
+              ),
+              textAlign: TextAlign.justify,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
